388bde079838e919359ff5da196e90ca92f6c5d4
akatsu14 <luong13166@gmail.com>
2025-04-20 08:34:18 +0700

    'room-and-dashboard'

--- backend/src/main/java/org/example/backend/controller/BlogController.java
+++ backend/src/main/java/org/example/backend/controller/BlogController.java
@@ -1,14 +1,14 @@
 package org.example.backend.controller;
 
+import java.math.BigDecimal;
 
-import lombok.RequiredArgsConstructor;
 import org.example.backend.service.BlogService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,57 +17,55 @@ public class BlogController {
 
     @GetMapping("/room/all")
     private ResponseEntity<?> getAllRoom(@RequestParam(required = false) String title,
-                                         @RequestParam(required = false) Boolean approve,
-                                         @RequestParam Integer pageNo,
-                                         @RequestParam Integer pageSize) {
-        return ResponseEntity.ok(blogService.getAllRoomForAdmin(title,approve, pageNo, pageSize));
+            @RequestParam(required = false) Boolean approve,
+            @RequestParam Integer pageNo,
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAllRoomForAdmin(title, approve, pageNo, pageSize));
     }
 
     @GetMapping("/customer/room")
     private ResponseEntity<?> getAllRoomForCustomer(
             @RequestParam(required = false) String title,
             @RequestParam(required = false) BigDecimal price,
-            @RequestParam(required = false) Long  categoryId,
+            @RequestParam(required = false) Long categoryId,
             @RequestParam Integer pageNo,
-            @RequestParam Integer pageSize
-    ){
-        return ResponseEntity.ok(blogService.getAllRoomForCustomer(title, price,categoryId, pageNo, pageSize));
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAllRoomForCustomer(title, price, categoryId, pageNo, pageSize));
     }
 
     // room order price asc
-    @GetMapping("/filter/order/price-asc")
+    @GetMapping("/blog/order/price-asc")
     public ResponseEntity<?> getRoomByPriceAsc(@RequestParam(required = false) String title,
-                                      @RequestParam(required = false) Boolean approve,
-                                      @RequestParam Integer pageNo,
-                                      @RequestParam Integer pageSize) {
-        return ResponseEntity.ok(blogService.getAdminRoomOrderByPriceAsc(title,approve,pageNo,pageSize));
+            @RequestParam(required = false) Boolean approve,
+            @RequestParam Integer pageNo,
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAdminRoomOrderByPriceAsc(title, approve, pageNo, pageSize));
     }
 
     // room order price desc
     @GetMapping("/blog/order/price-desc")
     public ResponseEntity<?> getRoomByPriceDesc(@RequestParam(required = false) String title,
-                                      @RequestParam(required = false) Boolean approve,
-                                      @RequestParam Integer pageNo,
-                                      @RequestParam Integer pageSize) {
-        return ResponseEntity.ok(blogService.getAdminRoomOrderByPriceDesc(title,approve,pageNo,pageSize));
+            @RequestParam(required = false) Boolean approve,
+            @RequestParam Integer pageNo,
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAdminRoomOrderByPriceDesc(title, approve, pageNo, pageSize));
     }
 
     // priority status: CHUA THUE
-    @GetMapping("/blog/order/status-acs")
+    @GetMapping("/blog/order/status-asc")
     public ResponseEntity<?> getRoomByStatusAsc(@RequestParam(required = false) String title,
-                                                @RequestParam(required = false) Boolean approve,
-                                                @RequestParam Integer pageNo,
-                                                @RequestParam Integer pageSize) {
-        return ResponseEntity.ok(blogService.getAdminRoomOrderByStatusAsc(title,approve,pageNo,pageSize));
+            @RequestParam(required = false) Boolean approve,
+            @RequestParam Integer pageNo,
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAdminRoomOrderByStatusAsc(title, approve, pageNo, pageSize));
     }
 
     // priority status: DA THUE
     @GetMapping("/blog/order/status-desc")
     public ResponseEntity<?> getRoomByStatusDesc(@RequestParam(required = false) String title,
-                                                @RequestParam(required = false) Boolean approve,
-                                                @RequestParam Integer pageNo,
-                                                @RequestParam Integer pageSize) {
-        return ResponseEntity.ok(blogService.getAdminRoomOrderByStatusDesc(title,approve,pageNo,pageSize));
+            @RequestParam(required = false) Boolean approve,
+            @RequestParam Integer pageNo,
+            @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(blogService.getAdminRoomOrderByStatusDesc(title, approve, pageNo, pageSize));
     }
 }
-
